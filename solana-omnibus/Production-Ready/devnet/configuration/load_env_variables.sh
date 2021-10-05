f4903d36e8ea403067cb458e9d15bebd88e2a1ea
hadzija <hadzijasale@gmail.com>
2021-10-05 11:44:05 +0200

    #configuration file added

---
+++ solana-omnibus/Production-Ready/devnet/configuration/load_env_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#configuration for dapp node
+export DAPP_NODE_COUNT=2
+
+#configuration for network node
+export NETWORK_NODE_COUNT=2
+
+#configuration for nginx
+export HOST_DOMAIN="localhost"
+
+export NODE_1_DOMAIN="foo.com"
+export NODE_2_DOMAIN="foo.com"
+export PORT_1=8898
+export PORT_2=8899
